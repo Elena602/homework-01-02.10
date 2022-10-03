09bfd3cd4570c0413a05afcec5ef485a3d4eafe1
Elena <lenalosyakova@gmail.com>
2022-10-03 23:56:33 +0300

    Checking the square of the number

---
+++ work1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int number2 = int.Parse(Console.ReadLine());
+int kvadrat = number2*number2;
+if (number1 == kvadrat)
+{
+    Console.WriteLine("Число1 является квадратом числа2");
+}
+else
+Console.WriteLine("Число1 не является квадратом числа2");
