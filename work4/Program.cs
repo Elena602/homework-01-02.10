5789530b27979163467cbe16525b740eca1c15ee
Elena <lenalosyakova@gmail.com>
2022-10-03 23:35:37 +0300

    Maximum of 3 numbers

---
+++ work4/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int number1, int number2, int number3)
+{
+    int result = number1;
+    if (number2 > number1) result = number2;
+    if (number3 > result) result = number3;
+    return result;
+}
+Console.WriteLine("Введите число1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число3: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int result = Max(number1, number2, number3);
+Console.WriteLine(result);
