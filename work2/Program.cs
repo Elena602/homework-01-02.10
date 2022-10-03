daef33e400b10ea5e8afcbbc0e0d62f0267e3ca1
Elena <lenalosyakova@gmail.com>
2022-10-04 00:39:27 +0300

    Day of the week

---
+++ work2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int DayOfTheWeekNumber = int.Parse(Console.ReadLine());
+
+if (DayOfTheWeekNumber == 1)
+{
+  Console.WriteLine("Понедельник");  
+}
+else if (DayOfTheWeekNumber == 2)
+{
+  Console.WriteLine("Вторник");  
+}
+else if (DayOfTheWeekNumber == 3)
+{
+  Console.WriteLine("Среда");  
+}
+else if (DayOfTheWeekNumber == 4)
+{
+  Console.WriteLine("Четверг");  
+}
+else if (DayOfTheWeekNumber == 5)
+{
+  Console.WriteLine("Пятница");  
+}
+else if (DayOfTheWeekNumber == 6)
+{
+  Console.WriteLine("Суббота");  
+}
+else if (DayOfTheWeekNumber == 7)
+{
+  Console.WriteLine("Воскресенье");  
+};
