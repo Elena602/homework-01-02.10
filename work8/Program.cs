d87242e94f3a21433d62d0d175f8670b2576c6f5
Elena <lenalosyakova@gmail.com>
2022-10-05 23:17:46 +0300

    2Number

---
+++ work8/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int number1 = number / 10;
+int second = number1 % 10;
+Console.WriteLine("Вторая цифра трехзначного числа: " + second);
