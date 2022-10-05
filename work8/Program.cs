979f0c7171cbbe3a5106335b14ce3a0e7fcd1a32
Elena <lenalosyakova@gmail.com>
2022-10-05 23:38:09 +0300

    delete second number

--- work8/Program.cs
+++ work8/Program.cs
@@ -1,5 +1,8 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine());
-int number1 = number / 10;
-int second = number1 % 10;
-Console.WriteLine("Вторая цифра трехзначного числа: " + second);
+int first = number / 100;
+int number2 = number / 10;
+int second = number2 % 10;
+int number3 = number % 100;
+int third = number3 % 10;
+Console.WriteLine("Двухзначное число: " + first + third);
