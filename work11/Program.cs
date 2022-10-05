a587ebdda9576bb7c1121666fc4adb486f2be369
Elena <lenalosyakova@gmail.com>
2022-10-06 00:45:33 +0300

    remainder

---
+++ work11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+int remainder = number1 % number2;
+if (number1 % number2 == 0)
+{
+    Console.WriteLine("Число 1 кратно числу 2");
+}
+else
+{
+    Console.WriteLine("Остаток: " + remainder);
+};
+    
