3f51bf56db3eca306ef358165707f0525fa42cb4
Elena <lenalosyakova@gmail.com>
2022-10-05 22:56:23 +0300

    3number

---
+++ work7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int number1 = number % 100;
+int third = number1 % 10;
+Console.WriteLine("Последняя цифра трехзначного числа: " + third);
