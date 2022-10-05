32ae6f2d38e8925a27224b3e111531b735267e32
Elena <lenalosyakova@gmail.com>
2022-10-05 21:58:24 +0300

    Ranges

---
+++ work03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Диапазоны для возможных координат: x > 0, y > 0");
+};
+if (number == 2)
+{
+    Console.WriteLine("Диапазоны для возможных координат: x < 0, y > 0");
+};
+if (number == 3)
+{
+    Console.WriteLine("Диапазоны для возможных координат: x < 0, y < 0");
+};
+if (number == 4)
+{
+    Console.WriteLine("Диапазоны для возможных координат: x > 0, y < 0");
+};
