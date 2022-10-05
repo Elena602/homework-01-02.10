4243e1f1e0ef2b0da75c2e96da5a37db467f1b43
Elena <lenalosyakova@gmail.com>
2022-10-05 20:01:21 +0300

    Week

---
+++ work01/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int DayOfTheWeekNumber = int.Parse(Console.ReadLine());
+
+if (DayOfTheWeekNumber == 6)
+{
+  Console.WriteLine("Выходной");  
+}
+ else if (DayOfTheWeekNumber == 7)
+{
+  Console.WriteLine("Выходной");  
+}
+else if (DayOfTheWeekNumber == 1)
+{
+  Console.WriteLine("Не выходной");  
+}
+else if (DayOfTheWeekNumber == 2)
+{
+  Console.WriteLine("Не выходной");  
+}
+else if (DayOfTheWeekNumber == 3)
+{
+  Console.WriteLine("Не выходной");  
+}
+else if (DayOfTheWeekNumber == 4)
+{
+  Console.WriteLine("Не выходной");  
+}
+else if (DayOfTheWeekNumber == 5)
+{
+  Console.WriteLine("Не выходной");  
+}
