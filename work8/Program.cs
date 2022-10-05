46eaab3a0ad2ce9e6ce4f324978e9fde05c0ae9c
Elena <lenalosyakova@gmail.com>
2022-10-05 23:49:58 +0300

    second number

--- work8/Program.cs
+++ work8/Program.cs
@@ -1,8 +1,5 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine());
-int first = number / 100;
-int number2 = number / 10;
-int second = number2 % 10;
-int number3 = number % 100;
-int third = number3 % 10;
-Console.WriteLine("Двухзначное число: " + first + third);
+int second = number / 10;
+int number2 = second % 10;
+Console.WriteLine("Вторая цифра трехзначного числа: " + number2);
