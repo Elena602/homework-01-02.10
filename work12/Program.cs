2dc1bdf9cab9c1d6443e07b51cafc8330b9d33a3
Elena <lenalosyakova@gmail.com>
2022-10-06 19:41:53 +0300

    Third Number

---
+++ work12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int lastNumber = (number % 10);
+if (number / 100 > 0)
+{
+    Console.WriteLine("Третья цифра числа это: " + lastNumber);
+}
+else 
+Console.WriteLine("У числа нет третьей цифры");
