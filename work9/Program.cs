aa6e3541cec176efc73e2b943efeea28b9ed8ac1
Elena <lenalosyakova@gmail.com>
2022-10-06 16:24:00 +0300

    Number 10;99

---
+++ work9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите двузначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second = number % 10;
+int first = number / 10;
+if (first > second)
+{
+   Console.WriteLine("Первая цифра больше: " + first); 
+}
+else if (second > first)
+{
+    Console.WriteLine("Вторая цифра больше: " + second);
+}
+else 
+{
+    Console.WriteLine("Обе цифры равны");
+}
