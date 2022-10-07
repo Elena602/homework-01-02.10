091a8805ad01a9b4d6b09f3025880e60a43c77a7
Elena <lenalosyakova@gmail.com>
2022-10-07 16:14:19 +0300

    even numbers from 1

---
+++ work6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int M = 1;
+
+while (M <= N)
+{
+   if (M % 2 == 0)
+   {
+    Console.WriteLine("Четные числа: " + M);
+   } 
+   M++;
+};
+
+
+
